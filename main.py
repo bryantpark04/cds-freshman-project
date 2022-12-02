a74051a38c3c3e690b166523a52df9ca7810156a
lisarli <lisaruiwen04@gmail.com>
2022-12-02 18:17:50 -0500

    Made streamlit UI

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from knn_recommender import KNNRecommender
+import streamlit as st
 
-user_ratings = {}
 
-while True:
-    try:
-        movie_id = int(input("Enter a movie ID (press enter to quit): "))
-        rating = int(input("Enter rating for movie " + str(movie_id) + ": "))
+if 'user_ratings' not in st.session_state:
+    st.session_state.user_ratings = {}
 
-        user_ratings[movie_id] = rating
-    except:
-        break
+st.title("Movie Recommendation System")
+st.text("CDS Onboarding Project")
+form = st.form(key="submit-form")
+movie_id = form.number_input(label = "Movie ID: ", min_value = 1, max_value = 193609)
+rating = form.number_input(label = "Movie rating: ", min_value = 1, max_value = 5)
+add = form.form_submit_button("Add to Ratings")
 
-print("KNN Movie Recommendations:")
 knn_rec = KNNRecommender()
-print(knn_rec.recommend(user_ratings))
+
+if add:
+    print("Adding " + str(movie_id) + " " + str(rating))
+    st.session_state.user_ratings[movie_id] = rating
+
+if st.button(label = "Recommend Movies"):
+    st.write("Your Recommended Movies: ")
+    knn_recs = knn_rec.recommend(st.session_state.user_ratings)
+    st.write(knn_recs)
